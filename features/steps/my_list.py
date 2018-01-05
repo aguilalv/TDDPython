dd7033ae383df2806722aca29eefbcb3ffe027b7
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-01-05 06:21:07 +0000

    Functional test to input 2 to-do items translated to behave

--- features/steps/my_list.py
+++ features/steps/my_list.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.keys import Keys
+import time
+
 @when(u'I visit "{url}"')
 def visit(context,url):
     context.browser.get(url)
@@ -10,5 +13,26 @@ def see_title_mentions(context,expected_title):
 @then(u'I will see the page header mentions "{expected_header}"')
 def see_header_mentions(context,expected_header):
     header = context.browser.find_element_by_tag_name('h1')
-    context.text.assertIn(expected_header,header)
+    context.test.assertIn(expected_header,header)
+
+@then(u'I will see a  text box and will be prompted to input "{expected_prompt}"')
+def see_text_box_with_ptompt(context,expected_prompt):
+    inputbox = context.browser.find_element_by_id('id_new_item')
+    placeholder = inputbox.get_attribute('placeholder')
+    context.test.assertEqual(placeholder,expected_prompt)
+
+@when(u'I type "{text_input}" into a text box and press enter')
+def type_text_and_press_enter(context,text_input):
+    inputbox = context.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys(text_input)
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+@then(u'I will see the now lists "{expected_item}" item in a table')
+def see_item_in_table(context,expected_item):
+    table = context.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    context.test.assertTrue(
+        any(row.text == '1: '+expected_list_element for row in rows)
+    )
 
